f1dc23094995d2c06d5ae180af6b9f4fec4739a3
Artiomtb <artiomtb@gmail.com>
2016-02-17 22:19:45 +0200

    Removed writer declaration

--- src/main/java/ua/rbolck/rader/servlets/PostServlet.java
+++ src/main/java/ua/rbolck/rader/servlets/PostServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class PostServlet extends HttpServlet {
 
@@ -18,9 +17,6 @@ public class PostServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PostDAOI postDAO = new PostDAOImpl();
         Post post = postDAO.get(1);
-
-        PrintWriter writer = resp.getWriter();
-
         req.setAttribute("post", post);
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("/post.jsp");
         requestDispatcher.forward(req, resp);
