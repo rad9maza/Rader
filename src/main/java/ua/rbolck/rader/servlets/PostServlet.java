5f33f790dc907b0e1f3057919af4a01214bf4365
Artiomtb <artiomtb@gmail.com>
2016-02-22 21:05:40 +0200

    Change PostServlet to can working with different id's

--- src/main/java/ua/rbolck/rader/servlets/PostServlet.java
+++ src/main/java/ua/rbolck/rader/servlets/PostServlet.java
@@ -1,5 +1,6 @@
 package ua.rbolck.rader.servlets;
 
+import org.apache.log4j.Logger;
 import ua.rbolck.rader.dao.PostDAOI;
 import ua.rbolck.rader.dao.PostDAOImpl;
 import ua.rbolck.rader.entity.Post;
@@ -13,12 +14,25 @@ import java.io.IOException;
 
 public class PostServlet extends HttpServlet {
 
+    private static final Logger log = Logger.getLogger(PostServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PostDAOI postDAO = new PostDAOImpl();
-        Post post = postDAO.get(1);
-        req.setAttribute("post", post);
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/post.jsp");
-        requestDispatcher.forward(req, resp);
+        int id = 1;
+        try {
+            id = Integer.parseInt(req.getParameter("id"));
+            Post post = postDAO.get(id);
+            if (post != null) {
+                req.setAttribute("post", post);
+                RequestDispatcher requestDispatcher = req.getRequestDispatcher("/post.jsp");
+                requestDispatcher.forward(req, resp);
+            } else {
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
+        } catch (NumberFormatException e) {
+            log.error("NumberFormatException present while get parameter id = " + req.getParameter("id"), e);
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
     }
 }
