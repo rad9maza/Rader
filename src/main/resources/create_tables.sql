257c3b65da2b8ef37ab74e5f88a216d621889847
Rodion Bolkunevych <rbolck@gmail.com>
2016-02-25 10:18:44 +0200

    Formatted code

--- src/main/resources/create_tables.sql
+++ src/main/resources/create_tables.sql
@@ -8,17 +8,21 @@ CREATE SEQUENCE auto_id_users;
 CREATE SEQUENCE auto_post_id;
 
 CREATE TABLE posts
-(post_id INT UNIQUE PRIMARY KEY DEFAULT nextval('auto_post_id'),
- post_parent_id INT,
- post_title VARCHAR(255),
- post_content VARCHAR(255),
- post_likes INT DEFAULT 0,
- post_dislikes INT DEFAULT 0,
- author_id INT,
- creation_date TIMESTAMP NOT NULL default now());
+(
+  post_id        INT UNIQUE PRIMARY KEY DEFAULT nextval('auto_post_id'),
+  post_parent_id INT,
+  post_title     VARCHAR(255),
+  post_content   VARCHAR(255),
+  post_likes     INT                    DEFAULT 0,
+  post_dislikes  INT                    DEFAULT 0,
+  author_id      INT,
+  creation_date  TIMESTAMP NOT NULL     DEFAULT now()
+);
 
 CREATE TABLE users
-(user_id INT UNIQUE PRIMARY KEY DEFAULT nextval('auto_id_users'),
- group_id INT,
- user_name VARCHAR(16),
- user_password VARCHAR(16));
+(
+  user_id       INT UNIQUE PRIMARY KEY DEFAULT nextval('auto_id_users'),
+  group_id      INT,
+  user_name     VARCHAR(16),
+  user_password VARCHAR(16)
+);
