1b5effbf86062c8833fed00bbe0a7bba2ca94619
Rodion Bolkunevych <rbolck@gmail.com>
2016-03-15 17:38:40 +0200

    Fix scroll

--- src/main/webapp/js/scroll.js
+++ src/main/webapp/js/scroll.js
@@ -1,20 +1,25 @@
-window.onscroll = function (ev) {
-    if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
-        addPosts()
-        console.log("Bottom of page");
-    }
-};
+$(function () {
+    $(window).on('scroll', function () {
 
+        if ((parseInt(window.innerHeight) + parseInt(window.scrollY.toFixed())) >= parseInt(document.body.scrollHeight)) {
+            console.log("Bottom of page");
+            addPosts();
+        }
+    });
+});
+var from = 10;
+var delta = 5
 function addPosts() {
     $.ajax({
         url: "ajax/post",
         data: {
             action: "getNextFivePosts",
-            fromPost: 10,
-            delta: 5
+            fromPost: from,
+            delta: delta
         },
         method: "POST",
         success: function ($data) {
+            from += delta;
             for (var i = 0; i < ($data.posts).length; i++) {
 
                 var tmpHTML = (
@@ -27,15 +32,15 @@ function addPosts() {
                             "<div style=\"float : right;\">" +
                                 "<form role=\"form\" id=\"removeId=" + $data.posts[i].id + "\" action=\"/post?action=remove&id=" + $data.posts[i].id + "\" method=\"post\">" +
                                     "<a onclick=\"like(" + $data.posts[i].id + ")\">" +
-                                        "<span class=\"badge likes" + $data.posts[i].id + "\">" + $data.posts[i].likes + "</span>" +
-                                        "<span class=\"glyphicon glyphicon-thumbs-up\"></span></a>" +
-                                    "<a onclick=\"dislike(" + $data.posts[i].id + ")\">" +
-                                        "<span class=\"badge dislikes" + $data.posts[i].id + "\">" + $data.posts[i].dislikes + "</span>" +
-                                        "<span class=\"glyphicon glyphicon-thumbs-down\"></span></a>" +
-                                    "<a href=\"/post?action=edit&id=" + $data.posts[i].id + "\">" +
-                                        "<span class=\"glyphicon glyphicon-pencil\"></span></a>" +
-                                    "<a href=\"#\" onclick=\"document.getElementById('removeId=" + $data.posts[i].id + "').submit()\">" +
-                                        "<span class=\"glyphicon glyphicon-trash\"></span></a>" +
+                                        " <span class=\"badge likes" + $data.posts[i].id + "\">" + $data.posts[i].likes + "</span>" +
+                                        " <span class=\"glyphicon glyphicon-thumbs-up\"></span> </a>" +
+                                    " <a onclick=\"dislike(" + $data.posts[i].id + ")\"> " +
+                                        " <span class=\"badge dislikes" + $data.posts[i].id + "\">" + $data.posts[i].dislikes + "</span>" +
+                                        " <span class=\"glyphicon glyphicon-thumbs-down\"></span> </a>" +
+                                    " <a href=\"/post?action=edit&id=" + $data.posts[i].id + "\">" +
+                                        " <span class=\"glyphicon glyphicon-pencil\"></span> </a>" +
+                                    " <a href=\"#\" onclick=\"document.getElementById('removeId=" + $data.posts[i].id + "').submit()\">" +
+                                        "<span class=\"glyphicon glyphicon-trash\"></span> </a>" +
                                 "</form>" +
                             "</div>" +
                         "</h3>" +
